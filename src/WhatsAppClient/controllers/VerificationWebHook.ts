3aa02f157cf9dc23ffe8603ec522b80c45b4b522
Tyrone <tyronemguni@gmail.com>
2024-05-03 00:52:50 +0200

    Verification webhook awaiting testing

--- src/WhatsAppClient/controllers/VerificationWebHook.ts
+++ src/WhatsAppClient/controllers/VerificationWebHook.ts
@@ -3,4 +3,7 @@ import express from "express";
 export function VerificationWebhook(
   req: express.Request,
   res: express.Response
-) {}
+) {
+  const challenge: string = req.body["hub.challenge"];
+  res.send(challenge).status(200);
+}
