6451356d66f0c261aa7825e900eaa0f5aa809a51
taku263 <tnakwaenda@gmail.com>
2024-05-15 21:45:48 +0200

    ZigExchangeRate scrapper but commente out the cron part

---
+++ src/Credex/cronJobs/ZigExchangeRate.ts
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cron = require('node-cron');
+const https = require('https');
+
+const url = 'https://www.rbz.co.zw/index.php';
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false, // To Ignore SSL errors in dev
+});
+
+async function fetchExchangeRates() {
+  try {
+    const { data } = await axios.get(url, { httpsAgent });
+    const parsedHtml= cheerio.load(data);
+
+    const rates: { currency: string; bid: string; ask: string; avg: string }[] = [];
+
+    parsedHtml('#baTab1 table tbody tr').each((index: Number, element: any) => {
+      const currency: string = parsedHtml(element).find('td').eq(0).text().trim();
+      const bid: string = parsedHtml(element).find('td').eq(1).text().trim();
+      const ask: string = parsedHtml(element).find('td').eq(2).text().trim();
+      const avg: string = parsedHtml(element).find('td').eq(3).text().trim();
+
+      if (currency && bid && ask && avg) {
+        rates.push({ currency, bid, ask, avg });
+      }
+    });
+    console.log(rates);
+    return rates;
+  } catch (error) {
+    console.error('Error fetching exchange rates:', error);
+    return [];
+  }
+}
+
+fetchExchangeRates();
+
+// // Schedule this
+// cron.schedule('0 0 * * *', () => {
+//   console.log('Fetching exchange rates...');
+//   fetchExchangeRates().then((rates) => {
+//     console.log('Exchange Rates:', rates);
+    
+//   });
+// });
+
+// // Start the cron job
+// console.log('Cron job started');
