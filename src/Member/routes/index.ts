dc2ee2b0c54dbfa1c4286d612e8b77b2a3f0900e
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-05-15 16:23:00 +0000

    authorize to transact for company

--- src/Member/routes/index.ts
+++ src/Member/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { apiVersionOneRoute } from "../..";
 import { CreateMemberController } from "../controllers/CreateMemberController";
 import { CreateCompanyController } from "../controllers/CreateCompanyController";
+import { AuthorizeForCompanyController } from "../controllers/AuthorizeForCompanyController";
 import { GetMemberController } from "../controllers/GetMemberController";
 import { GetMembersListController } from "../controllers/GetMembersListController";
 import { UpdateMemberController } from "../controllers/UpdateMemberController";
@@ -21,6 +22,12 @@ export default function MemberRoutes(
     `${apiVersionOneRoute}createCompany`,
     jsonParser,
     CreateCompanyController
+  );
+
+  app.post(
+    `${apiVersionOneRoute}authorizeForCompany`,
+    jsonParser,
+    AuthorizeForCompanyController
   );
 
   // List view endpoint 
