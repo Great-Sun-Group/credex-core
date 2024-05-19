e20c4aa475320756eb5bbe83a71deb2617c91b15
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-05-19 14:14:15 +0000

    getMemberByPhone

---
+++ src/Member/services/GetMemberByPhoneService.ts
@@ -0,0 +1,35 @@
+/*
+returns member data
+
+required input:
+    phone
+
+returns object with fields for each member property
+
+returns null if member can't be found
+*/
+
+import { ledgerSpaceDriver } from "../../config/neo4j/neo4j";
+
+export async function GetMemberByPhoneService(phone: number): Promise<any | null> {
+    const ledgerSpaceSession = ledgerSpaceDriver.session();
+
+    try {
+        const result = await ledgerSpaceSession.run(`
+            MATCH (member:Member { phone: $phone })
+            RETURN member
+        `, { phone });
+
+        if (!result.records[0].length) {
+            return null;
+        }
+
+        return result.records[0].get("member").properties;
+        
+    } catch (error) {
+        console.error("Error fetching member data:", error);
+        return null;
+    } finally {
+        await ledgerSpaceSession.close();
+    }
+}
