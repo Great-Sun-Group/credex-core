2d8b75001ad667dd7aa3afc1555f2941219770d0
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-06-04 18:10:56 +0000

    AcceptCredexService

--- src/Credex/services/AcceptCredexService.ts
+++ src/Credex/services/AcceptCredexService.ts
@@ -1,31 +1,45 @@
+/*
+marks a credex as accepted by changing the relationships
+from OFFERS or REQUESTS to OWES
+
+required inputs:
+    credexID
+
+on success returns credexID
+
+will return false if:
+    credex does not have OFFERS or REQUESTS relationships (credex already accepted)
+
+*/
+
 import { ledgerSpaceDriver } from "../../config/neo4j/neo4j";
 
 export async function AcceptCredexService(credexID: string) {
+  const ledgerSpaceSession = ledgerSpaceDriver.session();
   try {
-    const ledgerSpaceSession = ledgerSpaceDriver.session();
     const result = await ledgerSpaceSession.run(
       `
-        MATCH (issuer:Member)-[rel1:OFFERS|REQUESTS]->(acceptedCredex:Credex{credexID:$credexID})-[rel2:OFFERS|REQUESTS]->(acceptor:Member)
+        MATCH (issuer:Member)-[rel1:OFFERS|REQUESTS]->(acceptedCredex:Credex {credexID: $credexID})-[rel2:OFFERS|REQUESTS]->(acceptor:Member)
         DELETE rel1, rel2
         CREATE (issuer)-[:OWES]->(acceptedCredex)-[:OWES]->(acceptor)
-        WITH acceptedCredex
-        SET
-            acceptedCredex.acceptedAt = Datetime()
+        SET acceptedCredex.acceptedAt = datetime()
         RETURN acceptedCredex.credexID AS credexID
-    `,
-      { credexID: credexID },
+      `,
+      { credexID }
     );
-    await ledgerSpaceSession.close();
 
-    if (result.records[0]) {
-      console.log(
-        "credex offer accepted for: " + result.records[0].get("credexID"),
-      );
-      return result.records[0].get("credexID");
+    const acceptedCredexID = result.records[0].get("credexID");
+    if (acceptedCredexID) {
+      console.log(`Offer accepted for credexID: ${acceptedCredexID}`);
+      return acceptedCredexID;
     } else {
+      console.log(`Error accepting offer for credexID: ${credexID}`);
       return false;
     }
   } catch (error) {
-    console.log(error);
+    console.error("Error accepting credex:", error);
+    throw error; // Optionally rethrow to allow further handling upstream
+  } finally {
+    await ledgerSpaceSession.close();
   }
 }
