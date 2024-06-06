55c4246b947b7b5e72096a5bb902a1924511a243
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-06-06 12:11:47 +0000

    offer, accept, decline, cancel credex documented

--- src/Credex/services/DeclineCredexService.ts
+++ src/Credex/services/DeclineCredexService.ts
@@ -1,3 +1,18 @@
+/*
+marks a credex as declined by changing the relationships
+from OFFERS or REQUESTS to DECLINED
+
+required inputs:
+    credexID
+
+on success returns credexID
+
+will return false if:
+    credexID not found
+    credex does not have OFFERS or REQUESTS relationships (credex already accepted/declined/cancelled)
+    
+*/
+
 import { ledgerSpaceDriver } from "../../config/neo4j/neo4j";
 
 export async function DeclineCredexService(credexID: string) {
@@ -15,15 +30,20 @@ export async function DeclineCredexService(credexID: string) {
             credex.queueStatus = "PROCESSED"
         RETURN credex.credexID AS credexID
     `,
-      { credexID: credexID },
+      { credexID }
     );
     await ledgerSpaceSession.close();
 
-    if (result.records[0]) {
-      return result.records[0].get("credexID");
-    } else {
+    if (result.records.length === 0) {
+      console.log(
+        `No records found or credex no longer pending for credexID: ${credexID}`
+      );
       return false;
     }
+
+    const declinedCredexID = result.records[0].get("credexID");
+    console.log(`Offer declined for credexID: ${declinedCredexID}`);
+    return declinedCredexID;
   } catch (error) {
     console.log(error);
   }
