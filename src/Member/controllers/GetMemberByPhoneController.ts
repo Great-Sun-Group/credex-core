d5a3b14a68c74662134431c9db7fc8638721d0b7
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-06-07 13:18:31 +0000

    add pending

--- src/Member/controllers/GetMemberByPhoneController.ts
+++ src/Member/controllers/GetMemberByPhoneController.ts
@@ -1,21 +1,33 @@
 import express from "express";
 import { GetMemberByPhoneService } from "../services/GetMemberByPhoneService";
 import { GetBalancesService } from "../../Credex/services/GetBalancesService";
+import { GetPendingOffersInService } from "../../Credex/services/GetPendingOffersInService";
+import { GetPendingOffersOutService } from "../../Credex/services/GetPendingOffersOutService";
 
 export async function GetMemberByPhoneController(
   req: express.Request,
   res: express.Response
 ): Promise<void> {
-
   try {
     const memberData = await GetMemberByPhoneService(req.body.phone);
-    let balanceData
+    let balanceData;
+    let pendingInData;
+    let pendingOutData;
     if (memberData) {
       balanceData = await GetBalancesService(memberData.memberID);
+      pendingInData = await GetPendingOffersInService(memberData.memberID);
+      pendingOutData = await GetPendingOffersOutService(memberData.memberID);
     }
 
     if (balanceData) {
-      res.status(200).json({memberData, balanceData});
+      res
+        .status(200)
+        .json({
+          memberData,
+          balanceData,
+          pendingInData,
+          pendingOutData
+        });
     } else {
       res.status(404).json({ message: "Member not found" });
     }
