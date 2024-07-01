5d19e651540d8f09925c786208165345055f7707
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-07-01 13:46:00 +0000

    return dashboard on acceptCredex

--- src/Credex/controllers/AcceptCredexController.ts
+++ src/Credex/controllers/AcceptCredexController.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import { AcceptCredexService } from "../services/AcceptCredexService";
+import { GetDashboardService } from "../../Member/services/GetDashboardService";
 
 export async function AcceptCredexController(
   req: express.Request,
   res: express.Response,
 ) {
   try {
-    const responseData = await AcceptCredexService(req.body.credexID);
-    res.json(responseData);
+    const acceptCredexData = await AcceptCredexService(req.body.credexID);
+    const dashboardData = await GetDashboardService(
+      req.body.memberID,
+      req.body.accountID
+    );
+    res.json({
+      acceptCredexData: acceptCredexData,
+      dashboardData: dashboardData,
+    });
   } catch (err) {
     res.status(500).json({ error: (err as Error).message });
   }
