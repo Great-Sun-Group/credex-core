e5b9299573e95e26f29579d6d1d37f3ae9ea9ccb
Takudzwanashe Nakwaenda <tnakw@tchipoyera-pc.pshc.co.zw>
2024-07-24 19:39:14 +0200

    Route protection middleware using bearer token and Api key

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import AdminRoutes from "./Admin/adminRoutes";
 import { Logger } from "../config/logger";
 import bodyParser from "body-parser";
 import startCronJobs from "./Core/cronJobs";
+import authenticate from "./middleware/authenticate";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const port = 5000;
@@ -14,7 +18,16 @@ const jsonParser = bodyParser.json();
 
 export const apiVersionOneRoute = "/api/v1/";
 
+app.use(helmet());
+app.use(cors());
 app.use(Logger);
+app.use(apiVersionOneRoute, authenticate);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 MemberRoutes(app, jsonParser);
 AccountRoutes(app, jsonParser);
--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -9,35 +9,48 @@ interface UserRequest extends Request {
 const authenticate = (req: UserRequest, res: Response, next: NextFunction) => {
   const authHeader = req.header('Authorization');
   const apiKeyHeader = req.header('x-api-key');
+  
 
-  // Check if Authorization header is present and starts with 'Bearer '
+  if (!process.env.JWT_SECRET) {
+    console.error('JWT_SECRET is not defined in environment variables');
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+
+  const handleForbidden = () => res.status(403).json({ message: 'Forbiddin' });
+  const handleUnauthorized = () => res.status(401).json({ message: 'Unauthorized' });
+
+  
   if (authHeader && authHeader.startsWith('Bearer ')) {
     const token = authHeader.split(' ')[1];
 
-    // Verify the JWT token
+    
     jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
       if (err) {
-        return res.status(403).json({ message: 'Forbidden' });
+        if (err.name === 'TokenExpiredError') {
+          return res.status(401).json({ message: 'Token expired' });
+        }
+        return handleForbidden();
       }
       req.user = user;
       next();
     });
   } 
-  // Fallback to API key authorization if Bearer token is not present
+  
   else if (apiKeyHeader) {
     const apiKey = apiKeyHeader;
+   
 
     // Validate the API key
     if (validateApiKey(apiKey)) {
       req.user = { apiKeyUser: true };
       next();
     } else {
-      return res.status(403).json({ message: 'Forbidden' });
+      return handleForbidden();
     }
   } 
   // If neither Authorization nor API key is present
   else {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return handleUnauthorized();
   }
 };
 
