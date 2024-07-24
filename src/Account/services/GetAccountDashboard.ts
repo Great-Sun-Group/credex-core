4c1d7ba2f4bbebf7087fdb37c6f9ab3df415a540
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-07-24 05:00:55 +0000

    get authFor with accountDashboard

--- src/Account/services/GetAccountDashboard.ts
+++ src/Account/services/GetAccountDashboard.ts
@@ -9,33 +9,72 @@ export async function GetAccountDashboardService(
 ) {
   const ledgerSpaceSession = ledgerSpaceDriver.session();
   try {
-    const result = await ledgerSpaceSession.run(
-      `
-        MATCH
-          (account:Account { accountID: $accountID })\
-          <-[:AUTHORIZED_FOR]-
-          (member:Member { memberID: $memberID})
-        RETURN
-          account.accountID AS accountID,
-          account.accountType AS accountType,
-          account.accountName AS accountName,
-          account.accountHandle AS accountHandle,
-          account.defaultDenom AS defaultDenom
-      `,
-      { memberID, accountID }
-    );
+interface AuthMember {
+  memberID: string;
+  firstname: string;
+  lastname: string;
+}
 
-    if (!result.records.length) {
-      console.log("account not found");
-      return null;
-    }
+interface AccountData {
+  accountID: string;
+  accountName: string;
+  accountHandle: string;
+  defaultDenom: string;
+  isOwnedAccount: boolean;
+  authFor: AuthMember[];
+  balanceData: any;
+  pendingInData: any;
+  pendingOutData: any;
+}
 
-    const accountData: any = {
-      accountID: result.records[0].get("accountID"),
-      accountName: result.records[0].get("accountName"),
-      accountHandle: result.records[0].get("accountHandle"),
-      defaultDenom: result.records[0].get("defaultDenom"),
-    };
+const result = await ledgerSpaceSession.run(
+  `
+    MATCH
+      (account:Account { accountID: $accountID })
+      <-[:AUTHORIZED_FOR]-
+      (member:Member { memberID: $memberID})
+    MATCH
+      (account)<-[:AUTHORIZED_FOR]-(allAuthMembers)
+    OPTIONAL MATCH
+      (account)<-[owns:OWNS]-(member)
+    RETURN
+      account.accountID AS accountID,
+      account.accountType AS accountType,
+      account.accountName AS accountName,
+      account.accountHandle AS accountHandle,
+      account.defaultDenom AS defaultDenom,
+      owns IS NOT NULL AS isOwnedAccount,
+      allAuthMembers.firstname AS authMemberFirst,
+      allAuthMembers.lastname AS authMemberLast,
+      allAuthMembers.memberID AS authMemberID
+  `,
+  { memberID, accountID }
+);
+
+if (!result.records.length) {
+  console.log("account not found");
+  return null;
+}
+
+const accountData: AccountData = {
+  accountID: result.records[0].get("accountID"),
+  accountName: result.records[0].get("accountName"),
+  accountHandle: result.records[0].get("accountHandle"),
+  defaultDenom: result.records[0].get("defaultDenom"),
+  isOwnedAccount: result.records[0].get("isOwnedAccount"),
+  authFor: [],
+  balanceData: [],
+  pendingInData: [],
+  pendingOutData: [],
+};
+
+result.records.forEach((record) => {
+  accountData.authFor.push({
+    memberID: record.get("authMemberID"),
+    firstname: record.get("authMemberFirst"),
+    lastname: record.get("authMemberLast"),
+  });
+});
 
     accountData.balanceData = await GetBalancesService(accountData.accountID);
     accountData.pendingInData = await GetPendingOffersInService(
