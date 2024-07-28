c73d7d43d2e55c706f1514ca9ecf450893a31b01
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-07-28 06:12:01 +0000

    only expose admin routes if DEPLOYMENT = demo

--- src/Admin/adminRoutes.ts
+++ src/Admin/adminRoutes.ts
@@ -11,44 +11,50 @@ import { GrowthTestController } from "./controllers/growthTest";
 import { CheckLedgerVsSearchBalancesController } from "./controllers/checkLedgerVsSearchBalances";
 
 export default function AdminRoutes(app: express.Application, jsonParser: any) {
-  app.delete(
-    `${apiVersionOneRoute}clearDevDB`,
-    jsonParser,
-    ClearDevDbController
-  );
+  if (process.env.DEPLOYMENT === "demo") {
+    app.delete(
+      `${apiVersionOneRoute}clearDevDB`,
+      jsonParser,
+      ClearDevDbController
+    );
 
-  app.post(`${apiVersionOneRoute}forceDCO`, jsonParser, ForceDcoController);
-  app.post(`${apiVersionOneRoute}forceMTQ`, jsonParser, ForceMtqController);
+    app.post(`${apiVersionOneRoute}forceDCO`, jsonParser, ForceDcoController);
+    app.post(`${apiVersionOneRoute}forceMTQ`, jsonParser, ForceMtqController);
 
-  app.post(
-    `${apiVersionOneRoute}offerAndAcceptCredex`,
-    jsonParser,
-    OfferAndAcceptCredexController
-  );
+    app.post(
+      `${apiVersionOneRoute}offerAndAcceptCredex`,
+      jsonParser,
+      OfferAndAcceptCredexController
+    );
 
-  app.post(
-    `${apiVersionOneRoute}createTestMembersAndAccounts`,
-    jsonParser,
-    CreateTestMembersAndAccountsController
-  );
+    app.post(
+      `${apiVersionOneRoute}createTestMembersAndAccounts`,
+      jsonParser,
+      CreateTestMembersAndAccountsController
+    );
 
-  app.post(
-    `${apiVersionOneRoute}createRandomFloatingCredexes`,
-    jsonParser,
-    CreateRandomFloatingCredexesController
-  );
+    app.post(
+      `${apiVersionOneRoute}createRandomFloatingCredexes`,
+      jsonParser,
+      CreateRandomFloatingCredexesController
+    );
 
-  app.post(
-    `${apiVersionOneRoute}createTestLoop`,
-    jsonParser,
-    CreateTestLoopController
-  );
+    app.post(
+      `${apiVersionOneRoute}createTestLoop`,
+      jsonParser,
+      CreateTestLoopController
+    );
 
-  app.post(`${apiVersionOneRoute}growthTest`, jsonParser, GrowthTestController);
+    app.post(
+      `${apiVersionOneRoute}growthTest`,
+      jsonParser,
+      GrowthTestController
+    );
 
-  app.get(
-    `${apiVersionOneRoute}checkLedgerVsSearchBalances`,
-    jsonParser,
-    CheckLedgerVsSearchBalancesController
-  );
+    app.get(
+      `${apiVersionOneRoute}checkLedgerVsSearchBalances`,
+      jsonParser,
+      CheckLedgerVsSearchBalancesController
+    );
+  }
 }
