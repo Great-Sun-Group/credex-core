098ee749259ca4f5252d0bba4afbd69c4bd66bac
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-07-31 14:54:24 +0000

    update auth

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import AdminRoutes from "./Admin/adminRoutes";
 import { Logger } from "../config/logger";
 import bodyParser from "body-parser";
 import startCronJobs from "./Core/cronJobs";
-import authenticate from "./middleware/authenticate";
+import authenticate from "../config/authenticate";
 import helmet from "helmet";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
@@ -25,7 +25,7 @@ app.use(apiVersionOneRoute, authenticate);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
