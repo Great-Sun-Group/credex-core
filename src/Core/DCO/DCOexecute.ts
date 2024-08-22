a94d4f9f432578a78940cfbbd5f87717a49d6252
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-08-22 16:18:08 +0000

    little DCOflag fix

--- src/Core/DCO/DCOexecute.ts
+++ src/Core/DCO/DCOexecute.ts
@@ -139,11 +139,7 @@ export async function DCOexecute() {
             requiredDenoms,
           }
         );
-        console.log("Aborting DCO and turning off DCOrunningNow flag");
-        await ledgerSpaceSession.run(`
-          MATCH (daynode:Daynode {Active: TRUE})
-          SET daynode.DCOrunningNow = false
-        `);
+        console.log("Aborting DCO");
         return false;
       }
 
@@ -444,11 +440,6 @@ export async function DCOexecute() {
   } catch (error) {
     console.error("Error during DCOexecute:", error);
   } finally {
-    console.log("Turning off DCOrunningNow flag");
-    await ledgerSpaceSession.run(`
-      MATCH (daynode:Daynode {Active: TRUE})
-      SET daynode.DCOrunningNow = false
-    `);
     await ledgerSpaceSession.close();
     await searchSpaceSession.close();
   }
