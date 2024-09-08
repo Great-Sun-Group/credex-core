374007765a47c4ea93690c67e7110b59e3b6d06b
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-09-08 06:27:15 +0000

    Remove build directory from git tracking

--- build/src/constants/accountTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.accountTypes = void 0;
-exports.checkPermittedAccountType = checkPermittedAccountType;
-exports.accountTypes = ["PERSONAL_CONSUMPTION", "BUSINESS", "CREDEX_FOUNDATION"];
-function checkPermittedAccountType(credexTypeToCheck) {
-    return exports.accountTypes.includes(credexTypeToCheck);
-}
-//# sourceMappingURL=accountTypes.js.map
