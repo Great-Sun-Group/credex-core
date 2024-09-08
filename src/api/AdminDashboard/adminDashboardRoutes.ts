2058a038812f938a7b04137aa96721913179115f
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-09-08 03:02:17 +0000

    remove middleware from AdminDashboard

--- src/api/AdminDashboard/adminDashboardRoutes.ts
+++ src/api/AdminDashboard/adminDashboardRoutes.ts
@@ -1,63 +1,53 @@
-import express from "express"
-import { apiVersionOneRoute } from "../../index"
-import { authMiddleware } from "./middleware/authMiddleware";
+import express from "express";
+import { apiVersionOneRoute } from "../../index";
 import { getCredexDetails } from "./controllers/CredexController";
-import { getMemberDetails, updateMemberTier } from "./controllers/MemberController";
-import { getAccountDetails, getReceivedCredexOffers, getSentCredexOffers } from "./controllers/AccountController";
-/*
-function logRoute(req: express.Request, res: express.Response, next: express.NextFunction) {
-  console.log("getCredexDetails route hit");
-  next();
-}
-*/
+import {
+  getMemberDetails,
+  updateMemberTier,
+} from "./controllers/MemberController";
+import {
+  getAccountDetails,
+  getReceivedCredexOffers,
+  getSentCredexOffers,
+} from "./controllers/AccountController";
 
-
-export default function AdminDashboardRoutes(app: express.Application, jsonParser:any){
-  app.get(`${apiVersionOneRoute}getCredexDetails`,
-    //logRoute,
-    jsonParser,     
+export default function AdminDashboardRoutes(
+  app: express.Application,
+  jsonParser: any
+) {
+  app.get(
+    `${apiVersionOneRoute}getCredexDetails`,
+    jsonParser,
     getCredexDetails
   );
 
-  app.get(`${apiVersionOneRoute}getMemberDetails`,
-    jsonParser,      
+  app.get(
+    `${apiVersionOneRoute}getMemberDetails`,
+    jsonParser,
     getMemberDetails
   );
 
-  
-  app.patch(`${apiVersionOneRoute}updateMemberTier`,
-    jsonParser,    
+  app.patch(
+    `${apiVersionOneRoute}updateMemberTier`,
+    jsonParser,
     updateMemberTier
   );
 
-  app.get(`${apiVersionOneRoute}getAccountDetails`,
-    jsonParser,     
+  app.get(
+    `${apiVersionOneRoute}getAccountDetails`,
+    jsonParser,
     getAccountDetails
   );
 
-  app.get(`${apiVersionOneRoute}getReceivedCredexOffers`,
-    jsonParser,      
+  app.get(
+    `${apiVersionOneRoute}getReceivedCredexOffers`,
+    jsonParser,
     getReceivedCredexOffers
   );
 
-  app.get(`${apiVersionOneRoute}getSentCredexOffers`,
-    jsonParser,    
+  app.get(
+    `${apiVersionOneRoute}getSentCredexOffers`,
+    jsonParser,
     getSentCredexOffers
   );
-
-  /*
-  app.get(`${apiVersionOneRoute}getAccountActivityLog`,
-    jsonParser, 
-    authMiddleware, 
-    getAccountActivityLog
-  );
-  */
-
-  /*
-  app.put(`${apiVersionOneRoute}updateMemberStatus`,
-    jsonParser, 
-    authMiddleware, 
-    updateMemberStatus
-  );
-  */
 }
