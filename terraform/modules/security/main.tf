a361c7d1834479050d27264a6d3acccd838553f7
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-06 19:18:35 +0000

    don't wait for certificate

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -31,10 +31,11 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = data.aws_route53_zone.selected.zone_id
 }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.credex_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# Comment out the certificate validation resource
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   certificate_arn         = aws_acm_certificate.credex_cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
 
 resource "aws_route53_record" "api" {
   zone_id = data.aws_route53_zone.selected.zone_id
