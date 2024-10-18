e66877f03e7d71f2e31e204ff32a75282b6e673f
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-18 20:38:23 +0000

    debug

--- terraform/neo4j/outputs.tf
+++ terraform/neo4j/outputs.tf
@@ -4,49 +4,46 @@ output "neo4j_instance_ips" {
 }
 
 output "neo4j_bolt_urls" {
-  value = [for instance in aws_instance.neo4j : "bolt://${instance.private_ip}:7687"]
+  value = [
+    for i, instance in aws_instance.neo4j : 
+    "bolt://${instance.private_ip}:7687"
+  ]
   description = "Neo4j Bolt URLs"
   sensitive   = true
 }
 
 output "neo4j_ledger_space_bolt_url" {
   value       = "bolt://${aws_instance.neo4j[0].private_ip}:7687"
-  description = "Neo4j Ledger Space Bolt URL"
+  description = "Neo4j Bolt URL for LedgerSpace"
   sensitive   = true
 }
 
 output "neo4j_search_space_bolt_url" {
   value       = "bolt://${aws_instance.neo4j[1].private_ip}:7687"
-  description = "Neo4j Search Space Bolt URL"
-  sensitive   = true
-}
-
-output "neo4j_private_key" {
-  value       = var.create_key_pair ? tls_private_key.neo4j_key[0].private_key_pem : "Key pair not created in this run"
-  description = "Private key for Neo4j instances"
+  description = "Neo4j Bolt URL for SearchSpace"
   sensitive   = true
 }
 
 output "neo4j_ledger_space_username" {
   value       = "neo4j${random_string.neo4j_username_suffix[0].result}"
-  description = "Neo4j Ledger Space username"
+  description = "Neo4j username for LedgerSpace"
   sensitive   = true
 }
 
 output "neo4j_search_space_username" {
   value       = "neo4j${random_string.neo4j_username_suffix[1].result}"
-  description = "Neo4j Search Space username"
+  description = "Neo4j username for SearchSpace"
   sensitive   = true
 }
 
 output "neo4j_ledger_space_password" {
   value       = random_string.neo4j_password[0].result
-  description = "Neo4j Ledger Space password"
+  description = "Neo4j password for LedgerSpace"
   sensitive   = true
 }
 
 output "neo4j_search_space_password" {
   value       = random_string.neo4j_password[1].result
-  description = "Neo4j Search Space password"
+  description = "Neo4j password for SearchSpace"
   sensitive   = true
 }
--- terraform/shared_resources/outputs.tf
+++ terraform/shared_resources/outputs.tf
@@ -1,49 +1,44 @@
 output "vpc_id" {
+  value       = data.aws_vpc.default.id
   description = "The ID of the VPC"
-  value       = local.vpc_id
 }
 
 output "subnet_ids" {
-  description = "The IDs of the subnets"
   value       = local.subnet_ids
+  description = "The IDs of the subnets"
 }
 
 output "alb_security_group_id" {
-  description = "The ID of the ALB security group"
   value       = aws_security_group.alb.id
+  description = "The ID of the ALB security group"
 }
 
 output "ecs_tasks_security_group_id" {
-  description = "The ID of the ECS tasks security group"
   value       = aws_security_group.ecs_tasks.id
+  description = "The ID of the ECS tasks security group"
 }
 
 output "neo4j_security_group_id" {
-  description = "The ID of the Neo4j security group"
   value       = aws_security_group.neo4j.id
-}
-
-output "alb_arn" {
-  description = "The ARN of the Application Load Balancer"
-  value       = aws_lb.credex_alb.arn
-}
-
-output "target_group_arn" {
-  description = "The ARN of the target group"
-  value       = aws_lb_target_group.credex_core.arn
-}
-
-output "alb_listener" {
-  description = "The ALB listener"
-  value       = aws_lb_listener.credex_listener
-}
-
-output "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate"
-  value       = aws_acm_certificate.credex_cert.arn
+  description = "The ID of the Neo4j security group"
 }
 
 output "alb_dns_name" {
-  description = "The DNS name of the Application Load Balancer"
   value       = aws_lb.credex_alb.dns_name
+  description = "The DNS name of the Application Load Balancer"
+}
+
+output "target_group_arn" {
+  value       = aws_lb_target_group.credex_core.arn
+  description = "The ARN of the target group"
+}
+
+output "alb_listener" {
+  value       = aws_lb_listener.credex_listener.arn
+  description = "The ARN of the ALB listener"
+}
+
+output "key_pair_name" {
+  value       = aws_key_pair.credex_key_pair.key_name
+  description = "The name of the created key pair"
 }
