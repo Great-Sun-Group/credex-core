444b4c778570f74bce5c01f9912f1d34e6a8532f
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-18 21:08:13 +0000

    fix

--- terraform/shared_resources/outputs.tf
+++ terraform/shared_resources/outputs.tf
@@ -39,6 +39,6 @@ output "alb_listener" {
 }
 
 output "key_pair_name" {
-  value       = data.aws_key_pair.existing_key_pair.key_name
-  description = "The name of the existing key pair"
+  value       = aws_key_pair.credex_key_pair.key_name
+  description = "The name of the created key pair"
 }
