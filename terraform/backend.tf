40fab0608eb95c9f44955e87538c7a81530cc372
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-19 17:40:30 +0000

    Simplify Terraform backend to local and update GitHub Actions workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "TERRAFORM_STATE_BUCKET_PLACEHOLDER"
-    key            = "TERRAFORM_STATE_KEY_PLACEHOLDER"
-    region         = "af-south-1"
-    dynamodb_table = "TERRAFORM_LOCK_TABLE_PLACEHOLDER"
-    encrypt        = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
