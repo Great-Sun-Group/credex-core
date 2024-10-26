4267e8694dec434f74cf25e6928d2fff28192429
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-26 18:32:55 +0000

    dns

--- terraform/modules/connectors/shared_resources/main.tf
+++ terraform/modules/connectors/shared_resources/main.tf
@@ -238,11 +238,39 @@ resource "aws_acm_certificate" "credex_cert" {
   tags = merge(var.common_tags, {
     Name = "credex-cert-${var.environment}"
   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+# Get the hosted zone for the domain
+data "aws_route53_zone" "domain" {
+  name = var.domain_base
+}
+
+# Create DNS records for certificate validation
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.credex_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.domain.zone_id
+}
+
+# Certificate validation
 resource "aws_acm_certificate_validation" "credex_cert" {
   certificate_arn         = aws_acm_certificate.credex_cert.arn
-  validation_record_fqdns = [for record in aws_acm_certificate.credex_cert.domain_validation_options : record.resource_record_name]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 # ALB Listener
