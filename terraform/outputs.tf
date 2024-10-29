a3fe8bff4ec4a6d4a088e775d580f9f392dbc48e
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-29 15:59:17 +0000

    quick build output debug

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,3 +23,24 @@ output "alb_security_group_id" {
   value       = module.connectors.alb_security_group_id
   description = "The ID of the ALB security group"
 }
+
+# ECS outputs
+output "ecs_cluster_arn" {
+  value       = module.app.ecs_cluster_arn
+  description = "The ARN of the ECS cluster"
+}
+
+output "ecs_task_definition_arn" {
+  value       = module.app.ecs_task_definition_arn
+  description = "The ARN of the ECS task definition"
+}
+
+output "ecs_service_name" {
+  value       = module.app.ecs_service_name
+  description = "The name of the ECS service"
+}
+
+output "ecs_service_id" {
+  value       = module.app.ecs_service_id
+  description = "The ID of the ECS service"
+}
