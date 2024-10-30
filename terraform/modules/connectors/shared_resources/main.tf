bc171a9cdaae8b14fc0a2070b6aef5acd3b4caca
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-10-30 14:50:43 +0000

    add CloudFront

--- terraform/modules/connectors/shared_resources/main.tf
+++ terraform/modules/connectors/shared_resources/main.tf
@@ -242,6 +242,62 @@ resource "aws_s3_bucket_policy" "docs" {
   })
 }
 
+# CloudFront distribution for docs
+resource "aws_cloudfront_distribution" "docs" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  aliases             = ["docs.${var.domain}"]
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.docs.website_endpoint
+    origin_id   = "S3-docs.${var.domain}"
+    
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-docs.${var.domain}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress              = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.credex_cert.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "docs-cloudfront-${var.environment}"
+  })
+}
+
 # Application Load Balancer (ALB)
 resource "aws_lb" "credex_alb" {
   name               = "credex-alb-${var.environment}"
@@ -331,14 +387,15 @@ resource "aws_route53_record" "alb" {
   }
 }
 
+# Update Route53 record for docs to point to CloudFront
 resource "aws_route53_record" "docs" {
   zone_id = data.aws_route53_zone.domain.zone_id
   name    = "docs.${var.domain}"
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.docs.website_endpoint
-    zone_id                = aws_s3_bucket.docs.hosted_zone_id
+    name                   = aws_cloudfront_distribution.docs.domain_name
+    zone_id                = aws_cloudfront_distribution.docs.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -529,4 +586,8 @@ output "docs_bucket_name" {
 
 output "docs_bucket_website_endpoint" {
   value = aws_s3_bucket_website_configuration.docs.website_endpoint
+}
+
+output "docs_cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.docs.domain_name
 }
