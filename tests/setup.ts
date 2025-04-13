c68b3f94a49e3d5b0519d8cdec635a77daf46f1c
Ryan Luke Watson <85743878+ryanlukewatson@users.noreply.github.com>
2025-04-13 14:50:48 +0000

    tests to deployed dev working

--- tests/setup.ts
+++ tests/setup.ts
@@ -120,14 +120,6 @@ instance.interceptors.request.use(
       const { token, ...rest } = config.data;
       config.data = rest;
     }
-    
-    // If token is in query params (for GET requests), move it to Authorization header
-    if (config.params && config.params.token) {
-      config.headers.Authorization = `Bearer ${config.params.token}`;
-      // Remove token from query params
-      const { token, ...rest } = config.params;
-      config.params = rest;
-    }
 
     return config;
   },
